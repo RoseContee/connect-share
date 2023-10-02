72ac0d17459a5dd57d615f660e4f68478a0f3024
rosecontee <rosecontee@outlook.com>
2023-10-02 11:17:09 +0100

    2023-10-02

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Setting;
 use App\Models\Shortcut;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
         $settings = Setting::getSetting();
         $shortcuts = Shortcut::active()->get();
         view()->share('settings', $settings);
